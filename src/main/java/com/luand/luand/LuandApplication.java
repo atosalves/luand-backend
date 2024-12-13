a3ca7a62e3c3cd1c0a35b0d109a32ad55482afd3
Atos Alves <atosalvesqueiroz@gmail.com>
2024-12-12 22:35:13 -0300

    chore: cria estrutura básica do Spring com dependências

---
+++ src/main/java/com/luand/luand/LuandApplication.java
@@ -0,0 +1,13 @@
+package com.luand.luand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luand/luand/LuandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luand.luand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
