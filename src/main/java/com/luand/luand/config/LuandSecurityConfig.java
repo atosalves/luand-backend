780ad53d91dccde7275b3012705105ade3ae7655
Atos Alves <atosalvesqueiroz@gmail.com>
2024-12-13 16:49:06 -0300

    feat: add config for security

---
+++ src/main/java/com/luand/luand/config/LuandSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.luand.luand.config;
+
+// @Configuration
+public class LuandSecurityConfig {
+    // // @Bean
+    // BCryptPasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
+
+    // // @Bean
+    // DaoAuthenticationProvider authenticationProvider() {
+    // DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+    // return auth;
+    // }
+
+}
