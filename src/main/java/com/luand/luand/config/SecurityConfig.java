32aeee4ea90512ad03d79d3d2a5c446ede691cc1
Atos Alves <atosalvesqueiroz@gmail.com>
2024-12-18 15:59:08 -0300

    fix: solve cors problem

--- src/main/java/com/luand/luand/config/SecurityConfig.java
+++ src/main/java/com/luand/luand/config/SecurityConfig.java
@@ -17,11 +17,15 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -43,14 +47,26 @@ public class SecurityConfig {
                 .authorizeHttpRequests(configurer -> configurer
                         .requestMatchers(HttpMethod.POST, "/login", "/users", "/error").permitAll()
                         // disable later
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
-                                "/swagger-resources/**", "/webjars/**")
-                        .permitAll()
+                        // .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
+                        // "/swagger-resources/**", "/webjars/**")
+                        // .permitAll()
                         .requestMatchers(HttpMethod.GET, "/models", "/models/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/items", "/items/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/fashion-lines", "/fashion-lines/**").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(
+                        new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.addAllowedOrigin("http://localhost:3000");
+                                config.addAllowedMethod("*");
+                                config.addAllowedHeader("*");
+                                config.setAllowCredentials(true);
+                                return config;
+                            }
+                        }))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
