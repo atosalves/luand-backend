2c42e51958300ccd12a85a17ccbd1fe15e5920e8
Atos Alves <atosalvesqueiroz@gmail.com>
2025-01-27 18:40:16 -0300

    refactor: simplify CORS configuration in SecurityConfig

--- src/main/java/com/luand/luand/config/SecurityConfig.java
+++ src/main/java/com/luand/luand/config/SecurityConfig.java
@@ -19,13 +19,12 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -46,29 +45,17 @@ public class SecurityConfig {
         return httpSecurity
                 .authorizeHttpRequests(configurer -> configurer
                         .requestMatchers(HttpMethod.POST, "/login", "/users", "/error").permitAll()
-                        // disable later
-                        // .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
-                        // "/swagger-resources/**", "/webjars/**")
-                        // .permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
+                                "/swagger-resources/**", "/webjars/**")
+                        .permitAll() // disable later
                         .requestMatchers(HttpMethod.GET, "/models", "/models/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/items", "/items/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/fashion-lines", "/fashion-lines/**").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(
-                        new CorsConfigurationSource() {
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration config = new CorsConfiguration();
-                                config.addAllowedOrigin("http://localhost:3000");
-                                config.addAllowedMethod("*");
-                                config.addAllowedHeader("*");
-                                config.setAllowCredentials(true);
-                                return config;
-                            }
-                        }))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
@@ -82,6 +69,18 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
         var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // modify this for production
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
