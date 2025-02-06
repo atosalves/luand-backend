750fbc521b7b4aba2015f297965fe0197beb0d02
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-06 18:05:01 -0300

    feat: add Docker support with Dockerfile and docker-compose, switch to PostgreSQL database

--- src/test/java/com/luand/luand/LuandApplicationTests.java
+++ src/test/java/com/luand/luand/LuandApplicationTests.java
@@ -1,13 +1,8 @@
 package com.luand.luand;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LuandApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
