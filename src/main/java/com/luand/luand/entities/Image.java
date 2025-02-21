b903a981ea19336d4e617774496c3ded1fd3f734
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-21 19:40:03 -0300

    feat: remover relacionamento com Print na entidade Image e adicionar CreateImageDTO com validações

--- src/main/java/com/luand/luand/entities/Image.java
+++ src/main/java/com/luand/luand/entities/Image.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,8 +28,5 @@ public class Image implements Serializable {
 
         @Column(unique = true)
         private String url;
-
-        @ManyToOne
-        private Print print;
 
 }
