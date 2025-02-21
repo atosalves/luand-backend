c4859e0155fb63942d98d158d43d5f0c3797b1e1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-21 19:30:31 -0300

    feat: atualizar UpdateItemDTO com novas validações e campos para referência e impressão

--- src/main/java/com/luand/luand/entities/dto/item/UpdateItemDTO.java
+++ src/main/java/com/luand/luand/entities/dto/item/UpdateItemDTO.java
@@ -1,17 +1,18 @@
 package com.luand.luand.entities.dto.item;
 
+import com.luand.luand.entities.Color;
 import com.luand.luand.entities.Size;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
 
 public record UpdateItemDTO(
-        @NotNull(message = "Size is required") Size size,
-        @NotBlank(message = "Color is required") @jakarta.validation.constraints.Size(min = 7, max = 9, message = "Color must be between 7 and 9 characters")
-        // TODO: alter later
-        String color,
-        @PositiveOrZero(message = "Available quantity must be zero or greater") @NotNull(message = "Available quantity is required") int availableQuantity,
-        @NotNull(message = "Fashion line is required") Long fashionLineDTO) {
+                @NotBlank(message = "{ref.not-blank}") @jakarta.validation.constraints.Size(max = 12, message = "{ref.size}") String ref,
+                @NotNull(message = "{size.not-null}") Size size,
+                @NotBlank(message = "{color.not-blank}") Color color,
+                @NotNull(message = "{available-quantity.not-null}") @Positive(message = "{available-quantity.positive}") int availableQuantity,
+                @NotNull(message = "{print-id.not-null}") Long printId) {
 
 }
+
