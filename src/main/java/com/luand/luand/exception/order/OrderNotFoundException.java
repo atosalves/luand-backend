a0253abcc6258bccad5ee4bd0f47d0b7de1dc64c
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-21 19:24:25 -0300

    refactor: remover controlador, entidade e serviços de pedido

--- src/main/java/com/luand/luand/exception/order/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.luand.luand.exception.order;
-
-public class OrderNotFoundException extends RuntimeException {
-        public OrderNotFoundException(String message) {
-                super(message);
-        }
-}
--- src/main/java/com/luand/luand/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luand.luand.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luand.luand.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
