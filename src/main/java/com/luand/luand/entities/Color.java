0126eca1f08a69498f4f917a96526bb346842619
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-21 19:30:06 -0300

    feat: adicionar entidade Color e DTO para criação de cor

---
+++ src/main/java/com/luand/luand/entities/Color.java
@@ -0,0 +1,35 @@
+package com.luand.luand.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_color")
+public class Color implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(unique = true)
+        private String color;
+
+        @Column
+        private String name;
+
+        @Column
+        private String ref;
+
+}
