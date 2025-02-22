2b2b241b369ed8617608c42083234d3e74dd582b
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-22 01:18:01 -0300

    test: adiciona testes para buscar modelo por ID

--- src/test/java/com/luand/luand/services/ModelServiceTest.java
+++ src/test/java/com/luand/luand/services/ModelServiceTest.java
@@ -3,12 +3,14 @@ package com.luand.luand.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import com.luand.luand.entities.Model;
 import com.luand.luand.entities.Size;
 import com.luand.luand.entities.dto.model.CreateModelDTO;
+import com.luand.luand.exceptions.custom_exception.model.ModelNotFoundException;
 import com.luand.luand.repositories.ModelRepository;
 
 
@@ -30,6 +33,49 @@ public class ModelServiceTest {
         
         @Autowired
         ModelService modelService;
+
+        @Test
+        void shouldReturnModelById() {
+                var createModelDTO = new CreateModelDTO(
+                                "name_test",
+                                "ref_test",
+                                "description_test",
+                                BigDecimal.valueOf(10),
+                                Set.of(Size.P, Size.M));
+
+                var model = new Model(createModelDTO);
+
+                var modelId = 1L;
+                model.setId(modelId);
+
+                when(modelRepository.findById(modelId)).thenReturn(Optional.of(model));
+
+                var result = modelService.getModelById(modelId);
+
+                assertNotNull(result);
+
+                assertEquals(modelId, result.getId());
+                assertEquals("name_test", result.getName());
+                assertEquals("ref_test", result.getRef());
+                assertEquals("description_test", result.getDescription());
+                assertEquals(BigDecimal.valueOf(10), result.getPrice());
+                assertEquals(Set.of(Size.P, Size.M), result.getSupportedSizes());
+
+                verify(modelRepository).findById(modelId);
+        }
+
+        @Test
+        void shouldThrowNotFoundModelWhenModelById() {
+                var modelId = 1L;
+
+                when(modelRepository.findById(modelId)).thenThrow(new ModelNotFoundException());
+
+                assertThrows(ModelNotFoundException.class, () -> {
+                        modelService.getModelById(modelId);
+                });
+
+                verify(modelRepository).findById(modelId);
+        }
 
         @Test
         void shouldReturnAllModels() {
