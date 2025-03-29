a93ead87ae7786300f63c3e96538b49ac642ce28
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-28 21:25:08 -0300

    feat: adiciona métodos de busca por atributos em ColorRepository e ImageRepository

--- src/main/java/com/luand/luand/repositories/ColorRepository.java
+++ src/main/java/com/luand/luand/repositories/ColorRepository.java
@@ -1,9 +1,16 @@
 package com.luand.luand.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luand.luand.entities.Color;
 
 public interface ColorRepository extends JpaRepository<Color, Long> {
 
+        Optional<Color> findByHexColor(String hexColor);
+
+        Optional<Color> findByName(String name);
+
+        Optional<Color> findByRef(String ref);
 }
