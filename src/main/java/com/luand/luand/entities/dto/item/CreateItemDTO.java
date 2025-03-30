fb90b65a9f99c78eef51113588524e9573b2a61b
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-30 20:29:47 -0300

    fix: altera validação de cor para não nula em CreateItemDTO e UpdateItemDTO

--- src/main/java/com/luand/luand/entities/dto/item/CreateItemDTO.java
+++ src/main/java/com/luand/luand/entities/dto/item/CreateItemDTO.java
@@ -3,13 +3,12 @@ package com.luand.luand.entities.dto.item;
 import com.luand.luand.entities.Color;
 import com.luand.luand.entities.Size;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CreateItemDTO(
                 @NotNull(message = "{size.not-null}") Size size,
-                @NotBlank(message = "{color.not-blank}") Color color,
+                @NotNull(message = "{color.not-null}") Color color,
                 @NotNull(message = "{available-quantity.not-null}") @Positive(message = "{available-quantity.positive}") int availableQuantity,
                 @NotNull(message = "{print-id.not-null}") Long printId) {
 
